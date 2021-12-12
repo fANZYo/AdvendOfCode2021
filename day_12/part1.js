2fc4a98a66b490a0b069341c006d230ee383d749
fANZYo <www.iehl@gmail.com>
2021-12-12 22:55:23 +0000

    Day 12

--- day_12/part1.js
+++ day_12/part1.js
@@ -1,12 +1,12 @@
 const input = require('./input.json');
 
 const graph = input.reduce((graph, connection) => {
-	const parts = connection.split('-');
+	const [a, b] = connection.split('-');
 
 	return {
 		...graph,
-		[parts[0]]: (graph[parts[0]] || []).concat(parts[1]),
-		[parts[1]]: (graph[parts[1]] || []).concat(parts[0]),
+		[a]: (graph[a] || []).concat(b),
+		[b]: (graph[b] || []).concat(a),
 	};
 }, {});
 
